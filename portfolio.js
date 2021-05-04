56e4f724defa418a5de089a8770c9e27f8c18e54
Ferlrios <rios.fernando1105@gmail.com>
2021-05-04 13:49:48 -0300

    Trabajé en las fuentes de la página, en los div de la seccion skills, arreglé algunas cosas del footer, y le añadí un blur al fondo del parallex

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,4 @@
+
 addEventListener('DOMContentLoaded',() => {
     const btnMenu = document.querySelector('.btn-menu')
     if (btnMenu){
@@ -7,4 +8,39 @@ addEventListener('DOMContentLoaded',() => {
 
         })
     }
+})
+
+let js = document.getElementById("js");
+let css = document.getElementById("css");
+let html = document.getElementById("html");
+let net = document.getElementById("net");
+let skillName = document.getElementById("skillName");
+
+js.addEventListener('mouseover', ()=>{
+    skillName.textContent = "Javascript";
+})
+js.addEventListener('mouseout', ()=>{
+    skillName.textContent = "";
+})
+
+
+css.addEventListener('mouseover', ()=>{
+    skillName.textContent = "CSS3";
+})
+css.addEventListener('mouseout', ()=>{
+    skillName.textContent = "";
+})
+
+html.addEventListener('mouseover', ()=>{
+    skillName.textContent = "HTML5";
+})
+html.addEventListener('mouseout', ()=>{
+    skillName.textContent = "";
+})
+
+net.addEventListener('mouseover', ()=>{
+    skillName.textContent = ".NET";
+})
+net.addEventListener('mouseout', ()=>{
+    skillName.textContent = "";
 })
