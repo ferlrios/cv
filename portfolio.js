854a29bee09d229a272ac153175e35b3425a91ab
Ferlrios <rios.fernando1105@gmail.com>
2021-05-19 18:36:45 -0300

    Incluido Jquery en la página

--- portfolio.js
+++ portfolio.js
@@ -1,46 +1,49 @@
+addEventListener("DOMContentLoaded", () => {
+  const btnMenu = document.querySelector(".btn-menu");
+  if (btnMenu) {
+    btnMenu.addEventListener("click", () => {
+      const menuItems = document.querySelector(".menu-items");
+      menuItems.classList.toggle("show");
+    });
+  }
+});
 
-addEventListener('DOMContentLoaded',() => {
-    const btnMenu = document.querySelector('.btn-menu')
-    if (btnMenu){
-        btnMenu.addEventListener('click',()=>{
-            const menuItems = document.querySelector('.menu-items')
-            menuItems.classList.toggle('show')
+let showSkill = (skill) => {
+  $("#skillName").text(skill);
+};
 
-        })
-    }
-})
+$("#js").on({
+  mouseenter: function () {
+    showSkill("JavaScript");
+  },
+  mouseleave: function () {
+    showSkill("");
+  },
+});
 
-let js = document.getElementById("js");
-let css = document.getElementById("css");
-let html = document.getElementById("html");
-let net = document.getElementById("net");
-let skillName = document.getElementById("skillName");
+$("#css").on({
+  mouseenter: function () {
+    showSkill("CSS");
+  },
+  mouseleave: function () {
+    showSkill("");
+  },
+});
 
-js.addEventListener('mouseover', ()=>{
-    skillName.textContent = "Javascript";
-})
-js.addEventListener('mouseout', ()=>{
-    skillName.textContent = "";
-})
+$("#html").on({
+  mouseenter: function () {
+    showSkill("HTML");
+  },
+  mouseleave: function () {
+    showSkill("");
+  },
+});
 
-css.addEventListener('mouseover', ()=>{
-    skillName.textContent = "CSS3";
-})
-css.addEventListener('mouseout', ()=>{
-    skillName.textContent = "";
-})
-
-html.addEventListener('mouseover', ()=>{
-    skillName.textContent = "HTML5";
-})
-html.addEventListener('mouseout', ()=>{
-    skillName.textContent = "";
-})
-
-net.addEventListener('mouseover', ()=>{
-    skillName.textContent = ".NET";
-})
-net.addEventListener('mouseout', ()=>{
-    skillName.textContent = "";
-})
-
+$("#net").on({
+  mouseenter: function () {
+    showSkill(".NET");
+  },
+  mouseleave: function () {
+    showSkill("");
+  },
+});
